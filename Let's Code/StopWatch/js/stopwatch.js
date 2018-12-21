2b30b78a8d6b12e7492dbbda98688da3cac6238a
Bl0101 <yacine.mahdid@live.com>
2018-12-21 17:40:58 -0500

    FINISH stopwatch code
    Need to add in some php now to save the file

--- Let's Code/StopWatch/js/stopwatch.js
+++ Let's Code/StopWatch/js/stopwatch.js
@@ -1,10 +1,13 @@
 var Timer;
 var Button;
+var Lap;
 var isStop;
+var current_path = 'C:\\Users\\biapt\\Documents\\GitHub\\CodeThisCodeThat\\Let\'s Code\\StopWatch';
 
 function initialize(){
   Timer = document.getElementById("time");
   Button = document.getElementById("button");
+  Lap = document.getElementById("lap_time");
   isStop = 0;
 }
 
@@ -25,6 +28,9 @@ function stop(){
   Timer.innerHTML = "Your Finale time is: " + Timer.innerHTML;
   Button.innerHTML = "Start";
   Button.setAttribute('onclick','start()');
+  while (Lap.firstChild) {
+    Lap.removeChild(Lap.firstChild);
+  }
 }
 
 // Function that will increment the number on the Timer by one
@@ -37,6 +43,15 @@ function increment(){
 
   // Recurse
   setTimeout(increment, 1000);
+}
+
+function laps(){
+  if(isStop == 0){
+    var node = document.createElement("P");                 // Create a <li> node
+    var textnode = document.createTextNode("Time: " + Timer.innerHTML);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    Lap.appendChild(node);
+  }
 }
 
 // Helper function
